1af0af9df964953e9581a7bdfb7c44cf1cfef506
Mark Koslow <markkoslow@Marks-MacBook-Pro.local>
2014-07-03 22:53:57 -0400

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Mark Koslow on 7/3/14.
+//  Copyright (c) 2014 MarkKoslow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Mark Koslow on 7/3/14.
+//  Copyright (c) 2014 MarkKoslow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
